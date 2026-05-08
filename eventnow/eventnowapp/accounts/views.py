34ad39cac18c139f2d0ccd5dd51d09cd0635155d
Samuel Josefano Kaloh <kalohsamuel@gmail.com>
2026-05-09 08:51:27 +1000

    feat: login and register CTA

--- eventnow/eventnowapp/accounts/views.py
+++ eventnow/eventnowapp/accounts/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def login(request):
     return render(request, "accounts/login.html")
 
-def register_(request):
+def register(request):
     return render(request, "accounts/register.html")
